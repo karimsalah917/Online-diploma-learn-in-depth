95de527eb86f297e6ae15eaf0373c8cc455f1cf5
karim <karimsalah917@gmail.com>
2023-09-20 03:21:52 +0300

    adding assignment 1

--- Assignments/homework1/ex4/ex4.c
+++ Assignments/homework1/ex4/ex4.c
@@ -5,4 +5,15 @@
  *      Author: karim
  */
 
-
+#include<stdio.h>
+int main(){
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+	float x,y,product;
+	printf("Enter two numbers:");
+	scanf("%f",&x);
+	scanf("%f",&y);
+	product=x*y;
+	printf("Produce = %F",product);
+	return 0;
+}
---
+++ Assignments/homework1/ex5/ex5.c
@@ -0,0 +1,8 @@
+/*
+ * ex5.c
+ *
+ *  Created on: Sep 20, 2023
+ *      Author: karim
+ */
+
+
