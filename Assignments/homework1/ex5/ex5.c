d0e8a5018d05533a03403aa06639d4ec2bbfdad6
karim <karimsalah917@gmail.com>
2023-09-20 03:27:36 +0300

    adding assignment 1

--- Assignments/homework1/ex5/ex5.c
+++ Assignments/homework1/ex5/ex5.c
@@ -5,4 +5,13 @@
  *      Author: karim
  */
 
-
+#include<stdio.h>
+int main(){
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+	char x;
+	printf("Enter a character:");
+	scanf("%c",&x);
+	printf("Asci value is :%d",x);
+	return 0;
+}
