c0026555f6311f3dd473f8badad5fea02aaf9521
karim <karimsalah917@gmail.com>
2023-09-20 03:43:11 +0300

    adding assignmnet 1

--- Assignments/homework1/ex7/ex7.c
+++ Assignments/homework1/ex7/ex7.c
@@ -4,5 +4,24 @@
  *  Created on: Sep 20, 2023
  *      Author: karim
  */
+#include<stdio.h>
+int main(){
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+	int x,y;
+	//Printf("Enter the first integer a");
+	printf("Enter integer a:");
+	scanf("%d",&x);
+	//Printf("Enter the second integer a");
+	printf("Enter integer b:");
+	scanf("%d",&y);
+	x=x+y;
+	y=x-y;
+	x=x-y;
+	printf("a=%d \n",x);
+	printf("b=%d \n",y);
+	return 0;
 
+
+}
 
