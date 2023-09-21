07194a8ca2e9f9be1aae7786dee1fdab92b0a0f0
karim <karimsalah917@gmail.com>
2023-09-22 02:04:56 +0300

    adding assignment 2

---
+++ Assignments/unit-2-lec-3-c-basics/homework2/ex5/ex5.c
@@ -0,0 +1,23 @@
+/*
+ * ex5.c
+ *
+ *  Created on: Sep 22, 2023
+ *      Author: karim
+ */
+
+#include <stdio.h>
+int main() {
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);
+
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        printf("%c is an alphabet.", c);
+    else
+        printf("%c is not an alphabet.", c);
+
+    return 0;
+}
+
