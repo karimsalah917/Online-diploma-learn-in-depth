15795f4dc092eb3a84f2e14a519895314c0c147a
karim <karimsalah917@gmail.com>
2023-09-21 22:26:58 +0300

    adding assignment 2

--- Assignments/homework2/ex8/ex8.c
+++ Assignments/homework2/ex8/ex8.c
@@ -10,5 +10,28 @@
  */
 #include<stdio.h>
 int main(){
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
 	int operation,x,y;
+	printf("Enter operator either + or - or * or / :");
+	scanf("%d",&operation);
+	printf("Enter two operands\n");
+	scanf("%d",&x);
+	scanf("%d",&y);
+
+	switch(operation)
+	{
+	case '+' :
+	{
+		printf("%d + %d =%d",x,y,x+y);
+	}
+	break;
+	case '-' :
+	{
+		printf("%d - %d =%d",x,y,x-y);
+
+	}
+	break ;
+	}
+return 0;
 }
